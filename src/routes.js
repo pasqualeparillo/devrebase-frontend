cdc210160f5e9fd97c7a81f8e7c5905473c67a3e
pasqualeparillo <patparillo@gmail.com>
2019-10-16 16:55:14 -0400

    ... wrapped rout with router

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import IndexPage from "./pages/index";
 
 const BaseRouter = () => (
-  <div>
-    <Route exact path="/" component={IndexPage} />
-  </div>
+  <Router>
+    <div>
+      <Route exact path="/" component={IndexPage} />
+    </div>
+  </Router>
 );
 export default BaseRouter;
