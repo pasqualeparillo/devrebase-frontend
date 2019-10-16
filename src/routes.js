1858bc487b81396b124474d0a6b79c8becd19c85
pasqualeparillo <patparillo@gmail.com>
2019-10-16 16:14:02 -0400

    fixed router issue

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import IndexPage from "./pages/index";
+import AboutPage from "./pages/about";
 
 const BaseRouter = () => (
   <div>
     <Route exact path="/" component={IndexPage} />
+    <Route exact path="/about" component={AboutPage} />
   </div>
 );
 export default BaseRouter;
