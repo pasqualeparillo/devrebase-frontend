e44cc968687e85c77f4ff8b8953b0aea06104852
pasqualeparillo <patparillo@gmail.com>
2019-10-16 16:28:29 -0400

    test possible routing netlify issue

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import IndexPage from "./pages/index";
-import AboutPage from "./pages/about";
 
 const BaseRouter = () => (
   <div>
     <Route exact path="/" component={IndexPage} />
-    <Route path="/about" component={AboutPage} />
   </div>
 );
 export default BaseRouter;
