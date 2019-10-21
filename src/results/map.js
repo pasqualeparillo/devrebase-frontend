75fe86972f782ca71e1cb15cc1d4c245a8e7b7e3
pasqualeparillo <patparillo@gmail.com>
2019-10-21 07:27:00 -0400

    babel macro, styled components

--- src/results/map.js
+++ src/results/map.js
@@ -1,20 +1,31 @@
-import React, { useContext } from 'react';
-import Card from './card';
-import { ResultsContext } from '../store/results';
-import useFetch from './fetch';
-export default function Map({ history }) {
-	const { results, loading } = useContext(ResultsContext);
-	if (history.location.pathname === '/') {
-		useFetch();
-	}
-	return (
-		<div className="flex flex-wrap -ml-2 ">
-			{loading && (
-				<React.Fragment>
-					<div>Loading...</div> {console.log(results)}
-				</React.Fragment>
-			)}
-			{results && results.map((data, id) => <Card key={id} title={data.title} id={data.id} body={data.body} />)}
-		</div>
-	);
-}
+import React, { useContext } from "react";
+import Card from "./card";
+import { ResultsContext } from "../store/results";
+import useFetch from "../hooks/fetch";
+const Map = React.memo(function Map({ history }) {
+  const { results, loading } = useContext(ResultsContext);
+  function handleOnClick(data) {
+    console.log(data);
+  }
+  useFetch();
+  return (
+    <div className="flex flex-wrap -ml-2 ">
+      {loading ? (
+        <React.Fragment>
+          <div>Loading...</div> {console.log(results)}
+        </React.Fragment>
+      ) : (
+        results.map((data, id) => (
+          <Card
+            handleOnClick={() => handleOnClick(data)}
+            key={id}
+            title={data.title}
+            id={data.id}
+            body={data.body}
+          />
+        ))
+      )}
+    </div>
+  );
+});
+export default Map;
