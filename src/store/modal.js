160049973dec65dc7428d78ce1a10896ce374fa9
patp0513 <patparillo@gmail.com>
2020-01-07 13:52:54 -0500

    Responsive styling

--- src/store/modal.js
+++ src/store/modal.js
@@ -2,7 +2,7 @@ import React, { createContext, useState } from "react";
 export const ModalContext = createContext();
 export function ModalProvider(props) {
   const [isFilterActive, setIsFilterActive] = useState(false);
-
+  const []
   return (
     <ModalContext.Provider
       value={{
